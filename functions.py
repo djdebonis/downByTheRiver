93a5d630eacb2e518509445f42cb5c57915247c6
David J DeBonis <davidjdebonis@gmail.com>
2020-07-20 19:07:04 -0600

    add random filter

--- functions.py
+++ functions.py
@@ -1,1 +1,18 @@
-
+def readData(infilePathAndName):
+    """
+    Takes in the preprocessed/precleaned wordset that I wrote and returns a list of lists
+    containing the words tokenized into sentences (lists) and words (elements of lists)
+    
+    
+    """
+    
+    with open(infilePathAndName, "r") as f:
+        string = f.read()
+        ls1 = string.split("\n")
+        finalLs = []
+        for index, string in enumerate(ls1):
+            subLs = string.split(" ")
+            finalLs.append(subLs)
+            
+    return(finalLs)
+    
