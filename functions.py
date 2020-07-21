c30ffd6354a09379d91566989898d671067b476a
David J DeBonis <davidjdebonis@gmail.com>
2020-07-20 22:27:17 -0600

    working through frequency counts and getting lost in sauce

--- functions.py
+++ functions.py
@@ -16,3 +16,19 @@ def readData(infilePathAndName):
             
     return(finalLs)
     
+def uniqueArr(lsOfAllWords):
+    
+    uniqueTokens, uniqueFirstOccurance, uniqueTokenCounts = np.unique(lsOfAllWords, return_index=True, return_counts=True) # how many unique words did I use in the writing of 
+    # this book? what were the counts?
+    uniqueArr = np.asarray((uniqueTokens,uniqueFirstOccurance,uniqueTokenCounts)).T # turns tuple into ndarray then .T transposes it over its axis
+    
+    return(uniqueArr)
+
+def uniqueDf(uniqueArr):
+    unique = pd.DataFrame(uniqueArr, columns=['token', 'indexFirstTokenOccur','frequency']) # convert to dataframe for easier wrangling
+    unique.frequency = unique.frequency.astype(int) # before this line was added, the count/frequency had at some point been converted to a string and so
+    # the sort values function was sorting it based upon the first value in a string (e.g. 1 comes before 9, so 1203 is smaller tahn 99)
+    unique = unique.sort_values(by='frequency', ascending = False)
+    #pring the head
+    
+    return(unique)
